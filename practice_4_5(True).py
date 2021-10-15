0402eccd25e33da37469932540edf4827405add0
Artem Petrochenko <petrochenko.av@edu.spbstu.ru>
2021-10-15 19:04:29 +0300

    Fix5 and add5true

---
+++ practice_4_5(True).py
@@ -0,0 +1,4 @@
+strs = 0
+for i in range(6):
+    strs = '*' + i*' ' + '*'
+    print(strs)
--- practice_4_5.py
+++ practice_4_5.py
@@ -1,5 +1,5 @@
 stars = input('Введите строку из пробелов, начиная и заканчивая символом * : ')
 strs = 0
-for i in range(0,len(stars)):
+for i in range(0, len(stars)-1):
     strs = '*' + i*' ' + '*'
     print(strs)
