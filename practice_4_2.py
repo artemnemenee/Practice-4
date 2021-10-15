56e80a46b08ef5e205439ede97915516adb8509b
Artem Petrochenko <petrochenko.av@edu.spbstu.ru>
2021-10-15 18:29:58 +0300

    1-4

---
+++ practice_4_2.py
@@ -0,0 +1,8 @@
+ch = 0
+summ = 0
+print('Вводите числа')
+while ch >= 0:
+    ch = int(input())
+    if ch >= 0:
+        summ += ch
+print(summ)
---
+++ practice_4_3.py
@@ -0,0 +1,6 @@
+print('Введите число:')
+fa =1
+ch = int(input())
+for i in range(1,ch+1):
+    fa *= i
+print('Факториал числа равен:', fa)
