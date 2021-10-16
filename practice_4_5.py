40ac9ec28cda0e7b933816885d300e356d7b0e2a
Artem Petrochenko <petrochenko.av@edu.spbstu.ru>
2021-10-16 03:25:01 +0300

    Исправлено 5 задание

--- practice_4_5.py
+++ practice_4_5.py
@@ -1,5 +1,11 @@
-stars = input('Введите строку из пробелов, начиная и заканчивая символом * : ')
-strs = 0
-for i in range(0, len(stars)-1):
-    strs = '*' + i*' ' + '*'
-    print(strs)
+for i in range(0,6):
+    print("*", end ="")
+    for n in range(i):
+        print(" ", end="")
+    print("*", end="\n")
+print("реверс", end="\n")
+for i in range(5,-1,-1):
+    print("*", end ="")
+    for n in range(i):
+        print(" ", end="")
+    print("*", end="\n")
